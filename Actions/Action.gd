eb4606bb38b0df0c5548dfe0022194985a7c9001
Nicholas Rodine <halfofastaple@gmail.com>
2019-06-13 16:00:57 -0400

    Added tooltips and adjusted when things are revealed, to help guide the player.

--- Actions/Action.gd
+++ Actions/Action.gd
@@ -12,6 +12,33 @@ export(Array, int) var ProducedAmount = [];
 func _ready():
 	if (!Engine.editor_hint):
 		self.visible = false;
+	self.hint_tooltip = get_details();
+
+func get_details():
+	return str("Requires: ", get_requirements(),
+			   "\nProduces: ", get_products());
+
+func get_requirements():
+	if (Requires.size() == 0): return "Nothing";
+	var req = "";
+	for i in range(Requires.size()):
+		var resource = Requires[i];
+		var amount = RequiredAmount[i];
+		req += str(resource, " x", amount);
+		if (i != Requires.size() - 1):
+			req += ", ";
+	return req;
+
+func get_products():
+	if (Produces.size() == 0): return "Nothing";
+	var req = "";
+	for i in range(Produces.size()):
+		var resource = Produces[i];
+		var amount = ProducedAmount[i];
+		req += str(resource, " x", amount);
+		if (i != Produces.size() - 1):
+			req += ", ";
+	return req;
 
 func _pressed():
 	emit_signal("action_pressed", Requires, RequiredAmount, Produces, ProducedAmount);
@@ -22,5 +49,5 @@ func _process(_delta):
 func check_resources(resources):
 	var has_resources = resources.has_resources(Requires, RequiredAmount);
 	self.disabled = !has_resources;
-	if (has_resources):
+	if (resources.resources_are_visible(Requires)):
 		self.visible = true;
--- Resources/Resources.gd
+++ Resources/Resources.gd
@@ -8,6 +8,12 @@ func has_resources(input, input_count):
 		if (child.count < amount): return false;
 	return true;
 
+func resources_are_visible(input):
+	for resource in input:
+		var child = get_node(resource);
+		if (child.count < 1): return false;
+	return true;
+
 func take_resources(input, input_count):
 	for i in range(input.size()):
 		var resource = input[i];
