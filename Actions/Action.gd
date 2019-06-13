180a465f9e8de0a0ab1814361d2813a23231cb9a
Nicholas Rodine <halfofastaple@gmail.com>
2019-06-13 14:42:15 -0400

    Fleshed out process for making an Axe.

--- Actions/Action.gd
+++ Actions/Action.gd
@@ -1,4 +1,6 @@
 extends Button
+tool
+
 signal action_pressed(input, input_count, output, output_count);
 
 export(Array, String) var Requires = [];
@@ -7,8 +9,18 @@ export(Array, int) var RequiredAmount = [];
 export(Array, String) var Produces = [];
 export(Array, int) var ProducedAmount = [];
 
+func _ready():
+	if (!Engine.editor_hint):
+		self.visible = false;
+
 func _pressed():
 	emit_signal("action_pressed", Requires, RequiredAmount, Produces, ProducedAmount);
 
+func _process(_delta):
+	self.text = name;
+
 func check_resources(resources):
-	self.disabled = !resources.has_resources(Requires, RequiredAmount);
+	var has_resources = resources.has_resources(Requires, RequiredAmount);
+	self.disabled = !has_resources;
+	if (has_resources):
+		self.visible = true;
--- Resources/Resource.gd
+++ Resources/Resource.gd
@@ -1,6 +1,17 @@
 extends VBoxContainer
+tool
+
+export(Texture) var ResourceTexture = null;
 
 var count = 0;
 
+func _ready():
+	if (!Engine.editor_hint):
+		self.visible = false;
+
 func _process(_delta):
+	$Name.text = self.name;
 	$Count.text = str(count);
+	$Image.texture = ResourceTexture;
+	if (count > 0):
+		self.visible = true;
